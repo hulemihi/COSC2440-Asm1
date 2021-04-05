3cca53a555567ed8b23e82a7bc60028e967692a4
hulemihi <hulemihi@gmail.com>
2021-04-05 16:09:58 +0700

    second

--- src/Asm1/Student.java
+++ src/Asm1/Student.java
@@ -1,5 +1,19 @@
 package Asm1;
 
 public class Student {
-
+	private String id;
+	private String name;
+	private String birthdate;
+	
+	//	Constructor
+	public Student(String id, String name, String birthdate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthdate = birthdate;
+	}
+	
+	public static void main (String args[]) {
+		System.out.println("helo");
+	}
 }
